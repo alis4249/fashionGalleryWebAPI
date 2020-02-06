75a51fdd7c85d153039c67514802e9399528ac72
adoring.alis4249@gmail.com <adoring.alis4249@gmail.com>
2020-02-06 13:09:30 +0545

    prod by category

--- js/tasks/product.js
+++ js/tasks/product.js
@@ -1,102 +1,135 @@
 $(document).ready(function() {
-   
+    var link=window.location.href;
+    var valu=link.split('?')[1];
 
-    // $.getJSON('http://localhost:8080/product/getall', function(res) {
+    if(valu=='Male'){
 
-    //     $.each(res, function(data) {
-    //         $('#user_data').append(
-    //             '<a href="user_table.html">'+
-    //                 '<div class="user-img"> '+
-    //                 '<img src="http://localhost:8080/assets/images/user/'+res[data].user_image+'" alt="user" class="img-circle">'+ 
-    //                 '<span class="profile-status online pull-right"></span> </div>'+
-    //                 '<div class="mail-contnet">'+
-    //                     '<h5>'+res[data].user_name+'</h5> <span class="mail-desc">'+res[data].user_email+'</span></div>'+
-    //             '</a>'
-    //         );
-
-    //     });
-    // })
-
+        $.getJSON('http://localhost:8080/product/getByCategory/Male', function(res) {
+            $.each(res, function(data) {
+              
+                $('#productslist').append(
+                    '<div class="col-lg-4 col-sm-6">'+
+                                    '<div class="product-item">'+
+                                        '<div class="pi-pic">'+
+                                            '<img src="http://localhost:8080/assets/images/user/'+res[data].product_image+'" alt="">'+
+                                            '<div class="icon">'+
+                                                '<i class="icon_heart_alt"></i>'+
+                                            '</div>'+
+                                            '<ul>'+
+                                                '<li class="w-icon active"><a href="#"><i class="icon_bag_alt"></i></a></li>'+
+                                                '<li class="quick-view"><a href="product.html?'+res[data]._id+'">+ Quick View</a></li>'+
+                                                '<li class="w-icon"><a href="#"><i class="fa fa-random"></i></a></li>'+
+                                            '</ul>'+
+                                        '</div>'+
+                                        '<div class="pi-text">'+
+                                            '<div class="catagory-name">'+res[data].product_tag+'</div>'+
+                                            '<a href="#">'+
+                                                '<h5>'+res[data].product_name+'</h5>'+
+                                            '</a>'+
+                                            '<div class="product-price">'+
+                                                '$'+res[data].product_price+''+
+                                            '</div></div></div></div>'
+                );
     
-    $.getJSON('http://localhost:8080/product/getByCategory/Male', function(res) {
-        var n=1;
-        $.each(res, function(data) {
-            if(n<=10){
-                $('#men-product').append(
-                    '<div class="product-item">'+
-                         '<div class="pi-pic">'+
-                             '<img src="http://localhost:8080/assets/images/product/'+res[data].product_image+'" alt="">'+
-                             '<div class="icon">'+
-                                 '<i class="icon_heart_alt"></i>'+
-                             '</div>'+
-                             '<ul>'+
-                                 '<li class="w-icon active"><a href="product.html/'+res[data]._id+'"><i class="icon_bag_alt"></i></a></li>'+
-                                 '<li class="quick-view"><a href="product.html/'+res[data]._id+'">+ Quick View</a></li>'+
-                                 '<li class="w-icon"><a href="product.html/'+res[data]._id+'"><i class="fa fa-random"></i></a></li>'+
-                             '</ul>'+
-                         '</div>'+
-                         '<div class="pi-text">'+
-                             '<div class="catagory-name">'+res[data].product_tag+'</div>'+
-                             '<a href="product.html/'+res[data]._id+'">'+
-                                 '<h5>'+res[data].product_name+'</h5>'+
-                             '</a>'+
-                             '<div class="product-price">'+
-                                 '£'+res[data].product_price+''+
-                             '</div>'+
-                         '</div>'+
-                    '</div> '
-                        );
-            }
-            n++;
+                
+            });
+        })
+    }else if(valu=='Female'){
 
-        });
-    })
+        $.getJSON('http://localhost:8080/product/getByCategory/Female', function(res) {
+            $.each(res, function(data) {
+              
+                $('#productslist').append(
+                    '<div class="col-lg-4 col-sm-6">'+
+                                    '<div class="product-item">'+
+                                        '<div class="pi-pic">'+
+                                            '<img src="http://localhost:8080/assets/images/user/'+res[data].product_image+'" alt="">'+
+                                            '<div class="icon">'+
+                                                '<i class="icon_heart_alt"></i>'+
+                                            '</div>'+
+                                            '<ul>'+
+                                                '<li class="w-icon active"><a href="#"><i class="icon_bag_alt"></i></a></li>'+
+                                                '<li class="quick-view"><a href="product.html?'+res[data]._id+'">+ Quick View</a></li>'+
+                                                '<li class="w-icon"><a href="#"><i class="fa fa-random"></i></a></li>'+
+                                            '</ul>'+
+                                        '</div>'+
+                                        '<div class="pi-text">'+
+                                            '<div class="catagory-name">'+res[data].product_tag+'</div>'+
+                                            '<a href="#">'+
+                                                '<h5>'+res[data].product_name+'</h5>'+
+                                            '</a>'+
+                                            '<div class="product-price">'+
+                                                '$'+res[data].product_price+''+
+                                            '</div></div></div></div>'
+                );
     
+                
+            });
+        })
+    }else if(valu=='Kids'){
 
-    $.getJSON('http://localhost:8080/product/getByCategory/Female', function(res) {
-        var n=1;
-        
-                $('#women-product').append(
-                   '<div class="product-slider owl-carousel" id="women-product">'+
-                   $.each(res, function(data) {
-
-                    if(n<=10){
-
-                        '<div class="product-item">'+
-                        '<div class="pi-pic">'+
-                            '<img src="http://localhost:8080/assets/images/product/'+res[data].product_image+'" alt="">'+
-                            '<div class="icon">'+
-                                '<i class="icon_heart_alt"></i>'+
-                            '</div>'+
-                            '<ul>'+
-                                '<li class="w-icon active"><a href="product.html/'+res[data]._id+'"><i class="icon_bag_alt"></i></a></li>'+
-                                '<li class="quick-view"><a href="product.html/'+res[data]._id+'">+ Quick View</a></li>'+
-                                '<li class="w-icon"><a href="product.html/'+res[data]._id+'"><i class="fa fa-random"></i></a></li>'+
-                            '</ul>'+
-                        '</div>'+
-                        '<div class="pi-text">'+
-                            '<div class="catagory-name">'+res[data].product_tag+'</div>'+
-                            '<a href="product.html/'+res[data]._id+'">'+
-                                '<h5>'+res[data].product_name+'</h5>'+
-                            '</a>'+
-                            '<div class="product-price">'+
-                                '£'+res[data].product_price+''+
-                            '</div>'+
-                        '</div>'+
-                   '</div> '
-
-                    }
-
-                    n++;
-                   
-                     })
-
-                   +'</div> '
-
-                  
-                        );
-       alert(n)
-    })
+        $.getJSON('http://localhost:8080/product/getByCategory/Kids', function(res) {
+            $.each(res, function(data) {
+              
+                $('#productslist').append(
+                    '<div class="col-lg-4 col-sm-6">'+
+                                    '<div class="product-item">'+
+                                        '<div class="pi-pic">'+
+                                            '<img src="http://localhost:8080/assets/images/user/'+res[data].product_image+'" alt="">'+
+                                            '<div class="icon">'+
+                                                '<i class="icon_heart_alt"></i>'+
+                                            '</div>'+
+                                            '<ul>'+
+                                                '<li class="w-icon active"><a href="#"><i class="icon_bag_alt"></i></a></li>'+
+                                                '<li class="quick-view"><a href="product.html?'+res[data]._id+'">+ Quick View</a></li>'+
+                                                '<li class="w-icon"><a href="#"><i class="fa fa-random"></i></a></li>'+
+                                            '</ul>'+
+                                        '</div>'+
+                                        '<div class="pi-text">'+
+                                            '<div class="catagory-name">'+res[data].product_tag+'</div>'+
+                                            '<a href="#">'+
+                                                '<h5>'+res[data].product_name+'</h5>'+
+                                            '</a>'+
+                                            '<div class="product-price">'+
+                                                '$'+res[data].product_price+''+
+                                            '</div></div></div></div>'
+                );
     
+                
+            });
+        })
+    }else{
+        $.getJSON('http://localhost:8080/product/getall', function(res) {
+            $.each(res, function(data) {
+              
+                $('#productslist').append(
+                    '<div class="col-lg-4 col-sm-6">'+
+                                    '<div class="product-item">'+
+                                        '<div class="pi-pic">'+
+                                            '<img src="http://localhost:8080/assets/images/user/'+res[data].product_image+'" alt="">'+
+                                            '<div class="icon">'+
+                                                '<i class="icon_heart_alt"></i>'+
+                                            '</div>'+
+                                            '<ul>'+
+                                                '<li class="w-icon active"><a href="#"><i class="icon_bag_alt"></i></a></li>'+
+                                                '<li class="quick-view"><a href="product.html?'+res[data]._id+'">+ Quick View</a></li>'+
+                                                '<li class="w-icon"><a href="#"><i class="fa fa-random"></i></a></li>'+
+                                            '</ul>'+
+                                        '</div>'+
+                                        '<div class="pi-text">'+
+                                            '<div class="catagory-name">'+res[data].product_tag+'</div>'+
+                                            '<a href="#">'+
+                                                '<h5>'+res[data].product_name+'</h5>'+
+                                            '</a>'+
+                                            '<div class="product-price">'+
+                                                '$'+res[data].product_price+''+
+                                            '</div></div></div></div>'
+                );
+    
+                
+            });
+        })
+    }
+           
 
 });
